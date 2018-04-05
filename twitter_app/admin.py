ad127e5ac0e544ac0c51b570a139650eaeac6cda
Demir Antay <demir99antay@gmail.com>
2018-04-05 14:31:11 +0300

    started doing repsonsivness

--- twitter_app/admin.py
+++ twitter_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ProfileSettingsModel, TweetModel, FollowModel, RetweetModel
 
-# Register your models here.
+admin.site.register(ProfileSettingsModel)
+admin.site.register(TweetModel)
+admin.site.register(FollowModel)
+admin.site.register(RetweetModel)
