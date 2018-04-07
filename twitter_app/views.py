a0028ecaccebc0fbf2abb8efb61615796b2ce4a3
Demir Antay <demir99antay@gmail.com>
2018-04-07 17:36:00 +0300

    wow

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -194,7 +194,6 @@ def profile_settings(request):
         settings_model = ProfileSettingsModel.objects.get(user=current_user)
         if settings_form.is_valid():
             #settings_model.profile_photo = settings_form.cleaned_data['profile_photo']
-            photo =
             settings_model.profile_photo = cloudinary.uploader.upload_resource(settings_form.cleaned_data['profile_photo'], eager = [
             {"width": 230, "height": 230, "crop": "pad"}, ])
             #image_file = BytesIO(settings_model.profile_photo)
