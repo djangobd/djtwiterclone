288436369037fdaa779a71173dec0f66cafed0d3
Demir Antay <demir99antay@gmail.com>
2018-04-07 16:50:13 +0300

    resizing

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -195,7 +195,7 @@ def profile_settings(request):
         if settings_form.is_valid():
             #settings_model.profile_photo = settings_form.cleaned_data['profile_photo']
             settings_model.profile_photo = cloudinary.uploader.upload_resource(settings_form.cleaned_data['profile_photo'])
-            image_file = BytesIO(settings_model.profile_photo.read())
+            image_file = BytesIO(settings_model.profile_photo)
             image = Image.open(image_file)
             image = image.resize((230, 230))
             image_file = BytesIO()
