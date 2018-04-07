0d6e44f063eb5637f9b4715ed8c99daffa7e7521
Demir Antay <demir99antay@gmail.com>
2018-04-07 17:46:59 +0300

    resizing

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -194,8 +194,13 @@ def profile_settings(request):
         settings_model = ProfileSettingsModel.objects.get(user=current_user)
         if settings_form.is_valid():
             #settings_model.profile_photo = settings_form.cleaned_data['profile_photo']
-            settings_model.profile_photo = cloudinary.uploader.upload_resource(settings_form.cleaned_data['profile_photo'], eager = [
-            {"width": 230, "height": 230, "crop": "pad"}, ])
+            photo = settings_form.cleaned_data['profile_photo']
+            image_file = BytesIO(photo)
+            image = Image.open(image_file)
+            image = image.resize((230, 230))
+            image_file = BytesIO()
+            image.save(image_file, 'JPEG', quality=90)
+            settings_model.profile_photo = cloudinary.uploader.upload_resource(photo)
             #image_file = BytesIO(settings_model.profile_photo)
             #image = Image.open(image_file)
             #image = image.resize((230, 230))
