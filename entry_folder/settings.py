e499d1db0fb97a7d3c8227c62a3b05c058143c67
Demir Antay <demir99antay@gmail.com>
2018-04-07 19:21:35 +0300

    changed debug to false

--- entry_folder/settings.py
+++ entry_folder/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o1vjieg--fi2el67c%u0&%&fvl(v(3njfxz=xp9dlpxmz28tev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
